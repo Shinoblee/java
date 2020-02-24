1235fee483583055ef1068b3c7ad94face79d7af
Lee Simon <shinoblee@gmail.com>
2020-02-24 17:41:16 -0500

    Completed Best Price Finder

---
+++ BestPriceFinder/src/com/shinoblee/Main.java
@@ -0,0 +1,12 @@
+package com.shinoblee;
+
+import com.shinoblee.executor.CompletableFuturesDemo;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CompletableFuturesDemo.show();
+    }
+}
